e00841f8ab06166eef170a2dea092dc96d7bbec6
kakashi <kakashi@kakashis-MacBook-Pro.local>
2021-03-16 18:15:13 +0700

    setup icon and package npm

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ read -p "ketik 1 untuk run - 2 untuk build - 3 untuk buka xcode :" pointing
 if [ "$pointing" = 1 ]
 then
   cd ./android && ./gradlew clean
-  cd ../ && react-native run-android
+  cd ../ && npx react-native run-android
 elif [ "$pointing" = 2 ]
 then
   cd ./android && ./gradlew clean && ./gradlew assembleRelease
